fbec7954b8721687433e2a0b6c79927cc1a14d20
RahatKarim1795 <rht.krt@gmail.com>
2024-10-19 14:37:06 +0600

    sync

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -6,6 +6,7 @@
     <title>{{ config('app.name', 'Laravel') }} - Home</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     
+    
 </head>
 <body class="antialiased">
     <div class="container mx-auto p-4">
