2957d759b90c75e0baaa863020be16f1811a7a56
AzwadBhuiyan <azwadbhuiyan23@gmail.com>
2024-10-25 22:30:36 +0600

    home page improvements

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -158,10 +158,12 @@
                         <p class="text-base">Join our community and make a difference! Lorem Ipsum is simply dummy text of
                             the printing and typesetting industry.</p>
 
-                    <button type="button"
-                        class="mt-4 text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2">
-                        Explore Now
-                    </button>
+                    <a href="{{ route('idea_board.index') }}">
+                        <button type="button"
+                            class="mt-4 text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2">
+                            Explore Now
+                        </button>
+                    </a>
 
 
 
