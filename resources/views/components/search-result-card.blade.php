d09a4306d87f72cf23a6ff7c428681a081921948
AzwadBhuiyan <azwadbhuiyan23@gmail.com>
2024-11-11 01:19:51 +0600

    search results

--- resources/views/components/search-result-card.blade.php
+++ resources/views/components/search-result-card.blade.php
@@ -1,5 +1,5 @@
 <div class="rounded-lg mb-4 overflow-hidden flex flex-col shadow-lg border border-gray-200">
-    <div class="p-3 flex items-center space-x-4">
+    <div class="p-3 flex items-center space-x-4 hover:bg-gray-100">
         @if($category === 'organizations')
             <a href="{{ route('profile.public', $user->url) }}" class="flex-shrink-0">
                 <img src="{{ asset($user->getLogoPath()) }}" 
@@ -27,7 +27,7 @@
                     {{ $user->Name }}
                 </a>
                 <div class="text-sm text-gray-500">
-                    Level: {{ $user->level ?? 'Beginner' }}
+                    Level: {{ $user->getLevel() }}
                 </div>
             </div>
         @endif
