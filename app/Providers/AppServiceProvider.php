d6377b627d1b6a8d6b906135ef04945de46a642f
RahatKarim1795 <rht.krt@gmail.com>
2024-11-27 14:47:17 +0600

    kormonno name

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        config(['app.name' => 'VolunteerHub']);
+        config(['app.name' => 'Kormonno']);
 
         if($this->app->environment('production')) {
             URL::forceScheme('https');
